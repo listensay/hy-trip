bce19b75193114deffb9b0def42ea74c5e185e71
听说 <2107228359@qq.com>
2023-08-13 21:14:32 +0800

    使用vant tabbar

--- src/components/tab-bar/tabbar.vue
+++ src/components/tab-bar/tabbar.vue
@@ -1,64 +1,18 @@
 <script setup>
-import tabbarData from '@/assets/data/tabbarData'
-import { getAsstesUrl } from '@/utils/getAsstes'
 import { ref } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-
 const currentIndex = ref(0)
-const router = useRouter()
-
-const itemClick = (index, item) => {
-  currentIndex.value = index
-  router.push(item.path)
-}
 </script>
 
 <template>
   <div class="tab-bar">
-    <template v-for="(item, index) in tabbarData" :key="item.path">
-      <div class="tab-bar-item" :class="{ active: index === currentIndex }" @click="itemClick(index, item)">
-        <img class="icon" v-if="currentIndex !== index" :src="getAsstesUrl(item.image)">
-        <img class="icon" v-else :src="getAsstesUrl(item.imageActive)">
-        <span class="text">{{ item.text }}</span>
-      </div>
-    </template>
+    <van-tabbar v-model="currentIndex" active-color="#ff9854" route>
+      <van-tabbar-item icon="home-o" to="/home">标签</van-tabbar-item>
+      <van-tabbar-item icon="like-o" to="/favor">收藏</van-tabbar-item>
+      <van-tabbar-item icon="orders-o" to="/order">订单</van-tabbar-item>
+      <van-tabbar-item icon="chat-o" to="/message">消息</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <style lang="less" scoped>
-.tab-bar {
-  display: flex;
-  justify-content: space-between;
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  height: 50px;
-  background-color: #fff;
-  border-top: 1px solid #f1f1f1;
-
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    .icon {
-      width: 32px;
-    }
-
-    .text {
-      margin-top: 3px;
-      font-size: 12px;
-    }
-
-    &.active{
-      .text {
-        color: var(--parmary-color);
-      }
-    }
-  }
-}
 </style>
-@/assets/data/tabbarData@/utils/getAsstes
